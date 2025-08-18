f8478893800808c3fd7d7a1555e1af01a508d3d3
Sharath Kumar <152316074+Sharath0Kumar@users.noreply.github.com>
2025-08-18 23:58:12 +0530

    Chapter 6 uploaded

---
+++ Chapter_6/conditional_expression.py
@@ -0,0 +1,33 @@
+# if else ladder
+a  = int(input("Enter your age : "))
+
+if (a>=18):
+    print("You are above the age of consent")
+
+else:
+    print("You are below the age of consent")
+
+print("End of Programm")
+
+
+
+# if elif else ladder
+
+a  = int(input("Enter your age : "))
+
+if (a>=18):
+    print("You are above the age of consent")
+
+elif(a<0):
+    print("You are entering an invalid age")
+
+elif(a==0):
+    print("You are entering an invalid age that is 0")
+
+
+else:
+    print("You are below the age of consent")
+
+print("End of Programm")
+
+
---
+++ Chapter_6/practice_set.py
@@ -0,0 +1,90 @@
+# qn no 1
+# a1 = int(input("Enter the number 1 : "))
+# a2 = int(input("Enter the number 2 : "))
+# a3 = int(input("Enter the number 3 : "))
+# a4 = int(input("Enter the number 4 : "))
+
+# if(a1>a2 and a1>a3 and a1>a4):
+#     print("Greatest number is a1 : ",a1 )
+# elif(a2>a1 and a2>a3 and a2>a4):
+#     print("Greatest number is a2 : ",a2 )
+# if(a3>a1 and a3>a2 and a3>a4):
+#     print("Greatest number is a3 : ",a3 )
+# if(a4>a1 and a4>a2 and a4>a3):
+#     print("Greatest number is a4 : ",a4 )
+
+
+# qn no 2
+# marks1 = int(input("Enter the marks 1 : "))
+# marks2 = int(input("Enter the marks 2 : "))
+# marks3 = int(input("Enter the marks 3 : "))
+
+# total_percentage = (100*(marks1 + marks2 + marks3))/300
+
+# if(total_percentage >= 40 and marks1>=33 and marks2>=33 and marks3>=33):
+#     print("You are pass!! :", total_percentage)
+# else:
+#     print("You failed , try again :", total_percentage)
+
+
+# qn no 3
+# p1 = "Make a lot of money"
+# p2 = "buy now"
+# p3 = "subscribe now"
+# p4 = "click this"
+
+# message = input("Enter the message : ")
+
+# if((p1 in message) or (p2 in message) or (p3 in message) or (p4 in message)):
+#     print("This message is spam")
+# else :
+#     print("This message is not spam")
+
+
+# qn no 4
+# username = input("Enter username :")
+
+# if(len(username)<10):
+#     print("Your username contains less than 10 characters")
+# else : 
+#     print("All is well !!")
+
+
+# qn no 5
+
+# l = ["Sharath", "Ram", "Bob", "Tom"]
+
+# name = input("Enter the name : ")
+
+# if(name in l):
+#     print("Your name is in list")
+# else:
+#     print("Your name is not in the list")
+
+
+# qn no 6
+# marks = int(input("Enter the marks : "))
+
+# if(marks<=100 and marks>=90):
+#     grade = "Ex"
+# elif(marks<90 and marks>=80):
+#     garde = "A"
+# elif(marks<80 and marks>=70):
+#     garde = "B"
+# elif(marks<70 and marks>=60):
+#     garde = "C"
+# elif(marks<60 and marks>=50):
+#     garde = "D"
+# elif(marks<50):
+#     garde = "F"
+
+# print("Your grade is :",garde)
+
+
+# qn no 7
+post = input("Enter the post message :")
+
+if("Sharath".lower() in post.lower()):
+    print("This is post is talking about sharath")
+else:
+    print("This is post is talking about sharath")
